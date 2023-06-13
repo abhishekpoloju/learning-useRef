78bb99747a32f07afb12f1532e37e1869ef536b7
abhishek.poloju <abhishek.poloju@gmail.com>
2023-06-13 15:03:21 +0530

    added scrolling accoring to the team

---
+++ src/components/Box.tsx
@@ -0,0 +1,18 @@
+import React, { useRef } from 'react'
+import { forwardRef, useImperativeHandle } from 'react'
+import { IncrementedRef } from '~/pages/useImperative'
+
+type BoxProps = {
+  size: string,
+  color: string
+}
+
+export const Box = forwardRef<IncrementedRef, BoxProps>(({size, color}, ref)=>{
+    const divRef=useRef<HTMLDivElement>(null)
+    useImperativeHandle(ref,()=>({
+      getYLocation:()=>divRef.current?.getBoundingClientRect().top,
+      current:divRef.current
+    }))
+    return(
+      <div className={`h-[${size}px] w-[${size}px] border-2 ${color} my-0 mx-auto`} ref={divRef}></div>)
+    })
---
+++ src/pages/useImperative.tsx
@@ -0,0 +1,28 @@
+import React, { useRef } from 'react'
+import { Box } from '~/components/Box'
+export type IncrementedRef={
+  getYLocation:()=> number | undefined
+  current: HTMLDivElement | null
+}
+const UseImperative = () => {
+  const refs=[useRef<IncrementedRef>(null), useRef<IncrementedRef>(null), useRef<IncrementedRef>(null)]
+  const goToBox=(position:number)=>{
+    console.log('go to box: ',refs[position]?.current?.current)
+    const boxTop=refs[position]?.current?.getYLocation();
+    window.scrollTo({top: boxTop,behavior: 'smooth'})
+  }
+  return (
+    <>
+      <div>
+        <button onClick={()=>goToBox(0)} className='mr-3'>go to 1st box</button>
+        <button onClick={()=>goToBox(1)} className='mr-3'>go to 2nd box</button>
+        <button onClick={()=>goToBox(2)}className=''>go to 3rd box</button>
+      </div>
+      <Box size='500' color='bg-red-500' ref={refs[0]}/>
+      <Box size='500' color='bg-blue-500' ref={refs[1]}/>
+      <Box size='500' color='bg-green-500' ref={refs[2]}/>
+    </>
+  )
+}
+
+export default UseImperative
